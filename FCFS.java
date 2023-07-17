e3d9e59a4accd939f2f1d1f3e995b87169204c70
Omkar Girish Wagholikar <104051137+Omkar-Wagholikar@users.noreply.github.com>
2023-07-17 12:56:00 +0530

    Update FCFS.java

--- FCFS.java
+++ FCFS.java
@@ -10,18 +10,17 @@ public class FCFS{
         float [] time = {300, 125,400,150,100};
         chart = new Chart(5, pid, time);
         chart.compute();
-        System.out.println("Hello world!");
     }
 }
 
 class Chart{
     static float [][] chart;
     static int TW = 0, TAT;
-    static int size;
+    int size;
 
     Chart(int size, int[] pid, float [] time){
         this.size = size;
-        chart = new float[size][2];
+        chart = new float[size][3];
 
         for(int i=0; i<size; i++){
             chart[i][0] = pid[i];
@@ -29,17 +28,42 @@ class Chart{
         }
     }
 
-    public static void compute(){
+    public void compute(){
         for(int i=0; i<size; i++){
-            System.out.println(chart[i][0] + " : "+chart[i][1]);
-
+            chart[i][2] = TW;
             TAT = TW;
+            System.out.println(chart[i][0] + " - "+chart[i][1] + " - " + chart[i][2]);
             TW += chart[i][1]; 
         }
-        System.out.println(TAT+" "+TW);
+        System.out.println("TW: "+TW);
+        System.out.println("TAT: "+TAT);
     }
 }
 
-class ChartOps{
+class Process{
+    static int pid;
+    static float time;
+    static int priority = 0;
+    static Boolean hasPriority;
+    Process(int pid, float time, int priority){
+        this.pid = pid;
+        this.time = time;
+        this.priority = priority;
+        hasPriority = true;
+    }
 
+    Process(int pid, float time){
+        this.pid = pid;
+        this.time = time;
+        hasPriority = false;
+    }
+
+    public static void print(){
+        if(hasPriority){
+            System.out.print(pid + "" + time +"" +priority);
+        }
+        else{
+            System.out.print(pid + "" + time +"" +priority);
+        }
+    }
 }
