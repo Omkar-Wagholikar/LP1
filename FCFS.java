c441203a8e90f28adee3076157ac8b563cdd3274
Omkar Girish Wagholikar <104051137+Omkar-Wagholikar@users.noreply.github.com>
2023-07-17 12:39:48 +0530

    Update FCFS.java

--- FCFS.java
+++ FCFS.java
@@ -1,18 +1,45 @@
 public class FCFS{
-    static int[][] chart;
+    
     int size;
     float time;
+    static Chart chart;
 
-    static void makeChart(int size){
-        chart = new int[size][size];
-    }
     public static void main(String [] args){
-        int size = 5;
-        makeChart(size);
+        
+        int [] pid = {1,2,3,4,5};
+        float [] time = {300, 125,400,150,100};
+        chart = new Chart(5, pid, time);
+        chart.compute();
         System.out.println("Hello world!");
     }
 }
 
 class Chart{
-    
+    static float [][] chart;
+    static int TW = 0, TAT;
+    static int size;
+
+    Chart(int size, int[] pid, float [] time){
+        this.size = size;
+        chart = new float[size][2];
+
+        for(int i=0; i<size; i++){
+            chart[i][0] = pid[i];
+            chart[i][1] = time[i];
+        }
+    }
+
+    public static void compute(){
+        for(int i=0; i<size; i++){
+            System.out.println(chart[i][0] + " : "+chart[i][1]);
+
+            TAT = TW;
+            TW += chart[i][1]; 
+        }
+        System.out.println(TAT+" "+TW);
+    }
+}
+
+class ChartOps{
+
 }
