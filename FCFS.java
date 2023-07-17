e975b60fc2a496dd98bbfaf431d4d1a5df005e6c
Omkar Girish Wagholikar <104051137+Omkar-Wagholikar@users.noreply.github.com>
2023-07-17 12:23:53 +0530

    Add files via upload

---
+++ FCFS.java
@@ -0,0 +1,18 @@
+public class FCFS{
+    static int[][] chart;
+    int size;
+    float time;
+
+    static void makeChart(int size){
+        chart = new int[size][size];
+    }
+    public static void main(String [] args){
+        int size = 5;
+        makeChart(size);
+        System.out.println("Hello world!");
+    }
+}
+
+class Chart{
+    
+}
