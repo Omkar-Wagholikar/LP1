c7473ba6963ade0453a5c570597fa29de501731f
Omkar Girish Wagholikar <104051137+Omkar-Wagholikar@users.noreply.github.com>
2023-07-24 12:44:06 +0530

    Update Main.java with Round robin code

--- LP1/Assignment_1_Scheduling/Main.java
+++ LP1/Assignment_1_Scheduling/Main.java
@@ -6,20 +6,28 @@ import LP1.Assignment_1_Scheduling.process.Process;
 import LP1.Assignment_1_Scheduling.scheduling.*;
 
 public class Main {
-    public static void main(String [] args){
-        
-        ArrayList<Process> processes = new ArrayList<Process>(); 
-        long[] burstTimes = { 300 , 125 , 400 , 150 , 100, 3 } ;
-        float[] processPriority = { 11 , 2 , 33 , 4 , 15 , 56 } ;
-        for( int i = 0 ; i < burstTimes.length ; i++ ) {
+    public static void main(String[] args) {
+
+        ArrayList<Process> processes = new ArrayList<Process>();
+
+        // long[] burstTimes = { 300, 125, 40, 150, 100, 3 };
+                            // p0  p1  p2  p3  p4   p5
+        long[] burstTimes = { 50, 100, 50, 50, 100, 150 };
+        float[] processPriority = { 11, 2, 33, 4, 15, 56 };
+
+        for (int i = 0; i < burstTimes.length; i++) {
             Process p = new Process("P" + Integer.toString(i), burstTimes[i], processPriority[i]);
-            processes.add( p );
+            processes.add(p);
         }
+
         System.out.println("Hello world");
-        FCFS scheduler = new FCFS(processes);
+
+        // FCFS scheduler = new FCFS(processes);
         // SJF scheduler = new SJF(processes);
         // Priority scheduler = new Priority(processes);
+        Round scheduler = new Round(processes, 50);
+
         scheduler.schedule();
-        scheduler.printScheduledSequence();
+        // scheduler.printScheduledSequence();
     }
 }
