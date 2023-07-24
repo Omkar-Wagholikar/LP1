86ede7ae9cc03a09ef34288579734fc262230965
Omkar Girish Wagholikar <104051137+Omkar-Wagholikar@users.noreply.github.com>
2023-07-24 12:45:33 +0530

    Update Round.java TODO: fix odd values drop bug

--- LP1/Assignment_1_Scheduling/scheduling/Round.java
+++ LP1/Assignment_1_Scheduling/scheduling/Round.java
@@ -1,5 +1,50 @@
 package LP1.Assignment_1_Scheduling.scheduling;
 
-public class Round {
-    
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import LP1.Assignment_1_Scheduling.process.Process;
+
+public class Round extends Scheduling {
+    float time_slice = 0;
+    protected ArrayList<Process> all_processes; 
+    Map<String, Integer> waiting_time = new HashMap<String, Integer>();
+
+    public Round(ArrayList<Process> all_processes, float time_slice) {
+        System.out.println("ROUND");
+        this.all_processes = all_processes;
+        this.time_slice = time_slice;
+    }
+
+    public void schedule() {
+        float new_burst = 4;
+        while(new_burst > 0){
+            single_iteration();
+            System.out.print("========="+ new_burst + "=========\n");
+            new_burst -=1;
+        }
+        for(Process p : scheduledProcesses){
+            System.out.println(p.getProcess().toString());
+        }
+    }
+
+    protected void single_iteration(){
+        float new_burst=0;
+        for(int i=0 ;i<all_processes.size(); i++){
+            // Process process = new Process(all_processes.get(i));
+            Process process = all_processes.get(i);
+            System.out.println(process.getName() + "\t" + process.getBurstTime() + "\t" + (process.getBurstTime() - time_slice));
+            new_burst = process.getBurstTime() - time_slice;
+            process.setBurstTime(new_burst);
+            
+            // if(new_burst >= 0){
+            //     scheduledProcesses.add(process);
+            // } else {
+            //     all_processes.remove(i);
+            // }
+
+            if(new_burst < 0) all_processes.remove(i);
+        }
+    }
 }
