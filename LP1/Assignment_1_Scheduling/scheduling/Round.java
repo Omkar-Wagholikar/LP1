e62966c3c9a6c63002c15d007eb48ecb96d6ce47
Omkar Girish Wagholikar <104051137+Omkar-Wagholikar@users.noreply.github.com>
2023-07-26 20:51:38 +0530

    Updated Round robin

--- LP1/Assignment_1_Scheduling/scheduling/Round.java
+++ LP1/Assignment_1_Scheduling/scheduling/Round.java
@@ -2,7 +2,10 @@ package LP1.Assignment_1_Scheduling.scheduling;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
+import java.util.Scanner;
 
 import LP1.Assignment_1_Scheduling.process.Process;
 
@@ -17,34 +20,42 @@ public class Round extends Scheduling {
         this.time_slice = time_slice;
     }
 
-    public void schedule() {
-        float new_burst = 4;
-        while(new_burst > 0){
-            single_iteration();
-            System.out.print("========="+ new_burst + "=========\n");
-            new_burst -=1;
-        }
-        for(Process p : scheduledProcesses){
-            System.out.println(p.getProcess().toString());
-        }
-    }
+    public void schedule()
+    {
+        System.out.println("\nImplementing Round Robin: \n\nSCHEDULING ORDER IS AS FOLLOWS: ");
 
-    protected void single_iteration(){
-        float new_burst=0;
-        for(int i=0 ;i<all_processes.size(); i++){
-            // Process process = new Process(all_processes.get(i));
-            Process process = all_processes.get(i);
-            System.out.println(process.getName() + "\t" + process.getBurstTime() + "\t" + (process.getBurstTime() - time_slice));
-            new_burst = process.getBurstTime() - time_slice;
-            process.setBurstTime(new_burst);
-            
-            // if(new_burst >= 0){
-            //     scheduledProcesses.add(process);
-            // } else {
-            //     all_processes.remove(i);
-            // }
-
-            if(new_burst < 0) all_processes.remove(i);
+        //calculating wait time
+        float waitime = 0;
+        int processed = 0, sum = 0;
+        float CPU_time;
+        
+        CPU_time = time_slice;
+        System.out.print("CPU cycle: "+ CPU_time);
+        
+        while(processed!= all_processes.size())
+        {
+            for(int i = 0; i< all_processes.size(); i++)
+            {
+                if(all_processes.get(i).getBurstTime() > CPU_time)
+                {   
+                    all_processes.get(i).setBurstTime(all_processes.get(i).getBurstTime() - CPU_time);
+                    waitime += CPU_time;
+                    totalWaitTime += CPU_time;
+                }
+                else if(all_processes.get(i).getBurstTime()>0)
+                {
+                    waitime+= all_processes.get(i).getBurstTime();
+                    sum+=waitime;
+                    all_processes.get(i).setBurstTime(all_processes.get(i).getBurstTime() - CPU_time);
+                    System.out.print("\nTask Completed!");
+                    System.out.println(all_processes.get(i).getProcess().toString());
+                    System.out.println("Turnaround time is: " + waitime);
+                    totalTurnAroundTime += waitime;
+                    processed++;
+                }
+            }
         }
+
+        System.out.println("\nAvg wait time: "+ ((sum-waitime)/all_processes.size()));
     }
 }
