aa8e41d129761522223c1d7219ac7f5db5150a48
Omkar Girish Wagholikar <104051137+Omkar-Wagholikar@users.noreply.github.com>
2023-07-27 12:36:37 +0530

    added Arrival time

--- LP1/Assignment_1_Scheduling/process/Process.java
+++ LP1/Assignment_1_Scheduling/process/Process.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Process {
     
     String name;
-    float turn_around_time, burst_time, wait_time, priority;
+    float turn_around_time, burst_time, wait_time, priority, arrival_times;
 
     public Process(String name, float burst_time){
         this.burst_time = burst_time;
@@ -18,12 +18,20 @@ public class Process {
         this.name = name;
         this.priority = priority;
     }
-    public Process(String name, float turn_around_time, float burst_time, float wait_time){
+    // public Process(String name, float turn_around_time, float burst_time, float wait_time){
+    //     this.burst_time = burst_time;
+    //     this.name = name;
+    //     this.turn_around_time = turn_around_time;
+    //     this.wait_time = wait_time;
+    // }
+
+    public Process(String name, float turn_around_time, float burst_time, float arrival_times){
         this.burst_time = burst_time;
+        this.arrival_times = arrival_times;
         this.name = name;
         this.turn_around_time = turn_around_time;
-        this.wait_time = wait_time;
     }
+
     public String getName(){
         return name;
     }
@@ -39,6 +47,14 @@ public class Process {
     public float getPriority(){
         return priority;
     }
+
+    public float getArrival(){
+        return arrival_times;
+    }
+    public void setArrival(float arrival_times){
+        this.arrival_times = arrival_times;
+    }
+
     public void setPriority(float priority){
         this.priority = priority;
     }
@@ -57,6 +73,7 @@ public class Process {
     public Map<String, String> getProcess(){
         Map<String, String> map = new HashMap<String,String>();
         map.put("name", name);
+        map.put("Arrival_time", Float.toString(arrival_times));
         map.put("burst_time", Float.toString(burst_time));
         map.put("turn_around_time", Float.toString(turn_around_time));
         map.put("wait_time", Float.toString(wait_time));
