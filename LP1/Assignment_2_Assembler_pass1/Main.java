533fc2c8bab830c14ff927bb304058bfe1e783f1
Omkar Girish Wagholikar <104051137+Omkar-Wagholikar@users.noreply.github.com>
2023-07-31 12:57:29 +0530

    Update Main.java

--- LP1/Assignment_2_Assembler_pass1/Main.java
+++ LP1/Assignment_2_Assembler_pass1/Main.java
@@ -1,1 +1,20 @@
 
+package Assignment_2_Assembler_pass1;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String [] args){
+        List<String> commands = FileHandler.readProgram();
+        List<String> ops =FileHandler.readOptab();
+
+        Map<String, List<String>> map = new HashMap<>();
+
+        StringTokenizer token = new StringTokenizer(ops.get(0));
+        System.out.println(ops.get(0)+ " -> "+token.nextToken()+ " "+ token.nextToken());
+    }
+}
